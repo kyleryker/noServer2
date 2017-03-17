cfa18fc62861055497cd7f52f6a8a6557627a58a
Mikhail Collins <kyleryker@gmail.com>
2017-03-17 16:47:43 -0600

    changed get to jsonp

--- mainSrv.js
+++ mainSrv.js
@@ -1,29 +1,29 @@
 angular.module('starFinder').service('mainSrv', function ($http){
 
   this.getCharacter = (charName) => {
-    return $http.get(`http://swapi.co/api/people/?search=${charName}`)
+    return $http.jsonp(`http://swapi.co/api/people/?search=${charName}`)
   }
 
   this.getPlanet = (planetName) => {
-    return $http.get(`http://swapi.co/api/planets/?search=${planetName}`)
+    return $http.jsonp(`http://swapi.co/api/planets/?search=${planetName}`)
   }
 
   this.getStarship = (shipName) =>{
   if (shipName){
-    return $http.get(`http://swapi.co/api/starships/?search=${shipName}`)}
+    return $http.jsonp(`http://swapi.co/api/starships/?search=${shipName}`)}
   }
 
   this.getVehicle = (vehicleName) =>{
     if (vehicleName){
-      return $http.get(`http://swapi.co/api/vehicles/?search=${vehicleName}`)}
+      return $http.jsonp(`http://swapi.co/api/vehicles/?search=${vehicleName}`)}
   }
 
   this.getFilm = (filmTitle) =>{
-    return $http.get(`http://swapi.co/api/films/?search=${filmTitle}`)
+    return $http.jsonp(`http://swapi.co/api/films/?search=${filmTitle}`)
   }
 
   this.getSpecies = (speciesName) =>{
-    return $http.get(`http://swapi.co/api/species/?search=${speciesName}`)
+    return $http.jsonp(`http://swapi.co/api/species/?search=${speciesName}`)
   }
 
 
